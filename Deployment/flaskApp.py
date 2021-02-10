dac474d47d61e96b03f549bd9e9b7cf5c2bd3fd9
David Calle <dcalleg@eafit.edu.co>
2021-02-10 12:09:23 -0500

    graphic with plotly

--- Deployment/flaskApp.py
+++ Deployment/flaskApp.py
@@ -1,44 +1,31 @@
 from flask import Flask, render_template, send_file, request, redirect, url_for
-from markupsafe import escape
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+import plotly
+import plotly.graph_objs as go
+import pandas as pd
 import numpy as np
-import io
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('ej.html', url="", data = None)
+    return render_template('ej.html', graphicData="", data = None)
 
 @app.route('/', methods=['POST', 'GET'])
 def index_img():
     if request.method == 'POST':
-        return render_template('ej.html', url=url_for('fig', data= request.form), data=request.form)
+        return render_template('ej.html', graphicData=create_plot(request.form), data=request.form)
     else:
         return render_template('ej.html', url="")
 
+def create_plot(data):
+    carreras = ['ingeniería', 'política', 'veterinaria', 'ulinaria', 'teología', 'jueguitos', 'sociología', 'música']
+    data = [
+        go.Bar(y = carreras, x = list(data.values()), orientation = 'h')
+    ]
 
-@app.route('/fig/<data>')
-def fig(data):
-    return send_file(create_image(data), mimetype='image/png')
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
 
+    return graphJSON
 
-def create_image(data):
-    fig = plt.figure(figsize=(10,5))
-    fig.patch.set_facecolor('gray')
-    np.random.seed = 12
-    carreras = ['ingeniería', 'política', 'veterinaria', 'ulinaria', 'teología', 'jueguitos']
-    y_pos = np.random.randint(10, size=len(carreras))
-    plt.barh(carreras,y_pos, height=1, color='#5c03f5')
-    ax = plt.gca()
-    ax.tick_params(axis=u'both', which=u'both',length=0)
-    ax.set_facecolor((0, 0, 0))
-    ax.grid(axis="x")
-    [i.set_color("white") for i in plt.gca().get_xticklabels()]
-    [i.set_color("white") for i in plt.gca().get_yticklabels()]
-    for spine in ax.spines:
-        ax.spines[spine].set_visible(False)
-    img = io.BytesIO()
-    fig.savefig(img, transparent=True)
-    img.seek(0)
-    return img
+if __name__ == "__main__":
+    app.run(debug=True)
